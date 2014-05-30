6139c6f2289c6240b1e4b3f52cffa328fe281a51
Armand L <armands.la@gmail.com>
2014-05-30 09:00:13 +0300

    Initial Commit

---
+++ FlickView/FVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FVAppDelegate.h
+//  FlickView
+//
+//  Created by Armands Lazdiņš on 30/05/14.
+//  Copyright (c) 2014 Armands Lazdiņš. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlickView
+//
+//  Created by Armands Lazdiņš on 30/05/14.
+//  Copyright (c) 2014 Armands Lazdiņš. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FVAppDelegate class]));
+    }
+}
